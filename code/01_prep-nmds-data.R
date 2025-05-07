47fa6409087594a0c5d9f01eca6c35879b14e0e6
vanichols <virginia.nichols@gmail.com>
2025-05-07 11:39:41 +0200

    add ex data

--- code/01_prep-nmds-data.R
+++ code/01_prep-nmds-data.R
@@ -7,15 +7,16 @@
 
 # install.packages("pak")
 pak::pak("vanichols/CENTSdata")
-
+library(CENTSdata)
+library(tidyverse)
 
 # 1. wrangle data ---------------------------------------------------------
 
-
+d <- cents_fallpctcover
 
 
 # 2. write prepared data --------------------------------------------------
 
 
-# d %>% 
-#   write_csv("data/tidy-nmds.csv")
+d %>%
+  write_csv("data/ex-tidy-nmds.csv")
